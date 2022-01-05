fb408e1392c9be3d89f7acce9a9f9a50c0e6bc51
NirzorSen123 <bwubts18108@brainwareuniversity.ac.in>
2022-01-05 19:14:35 +0600

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_form = 'nirzor255sen@gmail.com';
+$email_form = 'mainakmondal78@gmail.com';
 
 $email_subject = 'New Form Submission';
 
