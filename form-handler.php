573c1034c45a90227b64519bbc984344977ba5e5
NirzorSen123 <bwubts18108@brainwareuniversity.ac.in>
2022-01-07 21:15:54 +0600

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,26 +1,62 @@
 <?php
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_form = 'nirzor255sen@gmail.com';
+$mailheader = "From:" .$name. "<" .$email. ">\r\n";
 
-$email_subject = 'New Form Submission';
+$recipient = "nirzor255sen@gmail.com";
 
-$email_body = "User Name: $name.\n". 
-              "User Email: $visitor_email.\n". 
-              "Subject: $subject.\n". 
-              "User Message: $message.\n";
+mail($recipient,$message,$mailheader) or die("Error!");
 
-$to = 'nirzor255sen@gmail.com';
+echo'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-$headers = "From: $email_from\r\n";
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous" />
+    <link rel="stylesheet" href="style.css" />
+    <script src="https://kit.fontawesome.com/b419a25ad3.js" crossorigin="anonymous"></script>
+    <title>Contact us</title>
 
-$headers = "Reply-To: $visitor_email \r\n";
+</head>
 
-mail($to,$email_subject,$email_body,$headers);
+<body>
+    
+            <div class="contact-col2">
+                <h1>Thank you for contact with us. We will get back to you as soon as possible.</h1>
+                <p class="back">Go back to the <a href="contact.html">Contact page</a>.</p>
+           </div>
+    
 
-header("location: contact.html");
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
+        </script>
+    <script src="script.js"></script>
+
+
+    
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+';
+
+
+
+
 
 ?>
